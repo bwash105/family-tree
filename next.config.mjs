f90e819bdb88199446540974ca52e1d34079fc25
Brian Washington <brianlewashington@gmail.com>
2026-06-04 21:49:44 +0300

    fix: use next.config.mjs (Next.js 14 does not support .ts config)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    loader: 'custom',
+    loaderFile: './src/supabase-image-loader.ts',
+  },
+}
 
-export default nextConfig;
+export default nextConfig
